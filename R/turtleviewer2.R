5a0299d2b08398dbe2598dd5ae68a0059533ee28
Florian Mayer <Florian.Mayer@dbca.wa.gov.au>
2022-03-08 08:45:06 +0800

    Declare global vars

---
+++ R/turtleviewer2.R
@@ -0,0 +1,21 @@
+#' @description `turtleviewer2` is an interactive dashboard for
+#' Western Australian turtle monitoring data,
+#' hosted at <https://turtledata.dbca.wa.gov.au/>.
+#'
+#' @keywords internal
+"_PACKAGE"
+
+# CMD check silencer
+utils::globalVariables(
+  c(
+    ".",
+    "latitude",
+    "longitude",
+    "observations",
+    "place_code",
+    "site_datum",
+    "site_latitude",
+    "site_longitude",
+    "sites"
+  )
+)
